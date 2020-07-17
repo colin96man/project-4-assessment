802a0008ee0a812793de3347710f13fb88e3fff1
Colin Nelson <colinnelson1996@gmail.com>
2020-07-17 14:37:32 -0600

    Updates

---
+++ precache-manifest.9e71a95b73431efdf39e7ba3de20d186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab03dbbf9966c0e923ab7dc7a406f406",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "0cf0e9991bd9445d37f8",
+    "url": "/project-4-assessment/static/css/main.866b61a8.chunk.css"
+  },
+  {
+    "revision": "2a420f8019b94e07307c",
+    "url": "/project-4-assessment/static/js/2.b2909b3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.b2909b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf0e9991bd9445d37f8",
+    "url": "/project-4-assessment/static/js/main.31793889.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
